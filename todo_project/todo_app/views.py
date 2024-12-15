b1931a37556ee3864d0296eaa6652adc54bd6325
bathulasrikanth <srikanthyadavbathula13@gmail.com>
2024-12-15 11:21:05 +0530

    TODO APP

---
+++ todo_project/todo_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+
+def Task_list(request):
+    tasks=Task.objects.all()
+    return render(request,"task_list.html",{"tasks":tasks})
+
+
+def add_task(request):
+    if request.method=="POST":
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'add_task.html', {'form': form})
+
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save() 
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'update_task.html', {'form': form})     
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'delete_task.html', {'task': task})
+ 
+
